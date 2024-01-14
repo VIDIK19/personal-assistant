e07f9641d9ff26991c7e802cb25478a60299162f
Oleksandr <kivilele@gmail.com>
2024-01-14 14:39:02 +0200

    Updated

--- main.py
+++ main.py
@@ -29,7 +29,43 @@ def main():
 
     # Підпарсер для note_manager
     note_parser = subparsers.add_parser('note', help='Керування нотатками')
-    # Тут можна додати команди та аргументи для note_manager
+    note_subparsers = note_parser.add_subparsers(dest='note_command')
+
+    # Додавання нової нотатки
+    create_note_parser = note_subparsers.add_parser('create', help='Додати нову нотатку')
+    create_note_parser.add_argument('info', type=str, help='Текст нотатки')
+
+    # Відкриття нотатки 
+    open_note_parser = note_subparsers.add_parser('open', help="Відкрити нотатки")
+    open_note_parser.add_argument('note_id', type=int, help='ID нотатки')
+
+    # Додавання тегу
+    add_tag_note_parser = note_subparsers.add_parser('add_tag', help='Додавання тегу')
+    add_tag_note_parser.add_argument('note_id', type=int, help="ID нотатки")
+    add_tag_note_parser.add_argument('tag', type=str, help='Назва тегу')
+
+    # Редагування тегу нотатки
+    edit_tag_note_parser = note_subparsers.add_parser('edit_tag', help='Редагування тегу' )
+    edit_tag_note_parser.add_argument('note_id', type=int, help='ID нотатки')
+    edit_tag_note_parser.add_argument('tag_index', type=int, help='Індекс тегу для редагування (від 0 з верху)')
+    edit_tag_note_parser.add_argument('new_tag', type=str, help='Нова назва тегу')
+
+    # Редагування тексту нотатки 
+    edit_info_note_parser = note_subparsers.add_parser('edit_info', help='Редагування тексту нотатки')
+    edit_info_note_parser.add_argument('note_id', type=int, help='ID нотатки')
+    edit_info_note_parser.add_argument('new_info', type=str, help='Новий текст нотатки')
+
+    # Показ БД нотаток
+    show_all_note_parser = note_subparsers.add_parser('show_all', help='Показати всі нотатки')
+
+    # Видалення нотатки
+    delete_note_parser = note_subparsers.add_parser('delete', help='Видаляє нотатку')
+    delete_note_parser.add_argument('note_id', type=int, help='ID нотатки')
+
+    # Пошук за тегом нотатки
+    search_note_parser = note_subparsers.add_parser('search', help='Пошук нотатки за тегом')
+    search_note_parser.add_argument('tag', type=str, help='Перші літери тегу нотатки')
+
 
     args = parser.parse_args()
 
@@ -49,40 +85,40 @@ def main():
         print(f"Файли у папці {args.path} відсортовано.")
 
     elif args.command == 'note':
-
         note_manager = NoteManager()
-
-        if args.note_command == 'add':
-            note_manager.add_note(args.content)
-            print("Нотатка додана.")
-
+        if args.note_command == 'create':
+            note_manager.add_note(args.info)
+            print('Нотатку створено')
+            note_manager.show_data()
+        elif args.note_command == 'open':
+            note_manager.open_note(args.note_id)
         elif args.note_command == 'add_tag':
-            # note_manager.add_tag(?, ?)
-            pass
-
+            note_manager.add_tag(args.note_id, args.tag)
+            print('Тег додано')
+            note_manager.show_data()
         elif args.note_command == 'edit_tag':
-            # note_manager.edit_tag(?, ?, ?) !
-            pass
-        
+            note_manager.edit_tag(args.note_id, args.tag_index, args.new_tag)
+            print('Тег змінено')
+            note_manager.show_data()
         elif args.note_command == 'edit_info':
-            # note_manager.edit_info(?, ?)
-            pass
-        
-        elif args.note_command == 'search_tag':
-            print(f"Всі співпадіння за тегом {args.content}:")
-            note_manager.search_tag(args.content)
-        
+            note_manager.edit_info(args.note_id, args.new_info)
+            print('Текст нотатки змінено')
+            note_manager.open_note(args.note_id)
         elif args.note_command == 'show_all':
             note_manager.show_data()
+        elif args.note_command == 'delete':
+            note_manager.delete_note(args.note_id)
+            print('Нотатку видалено')
+            note_manager.show_data()
+        elif args.note_command == 'search':
+            note_manager.search_tag(args.tag)
+
+
+            
+
+
         
-        elif args.note_command == 'open_note':
-            note_manager.open_note(args.content)
 
-        elif args.note_command == 'delete_note':
-            note_manager.delete_note(args.content)
-
-        elif args.note_command == 'delete_all_notes':
-            note_manager.clear_data()
 
     
 if __name__ == '__main__':

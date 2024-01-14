e356bd741b35637e3268502b95fd86ae75addcb1
VIDIK19 <125662034+VIDIK19@users.noreply.github.com>
2024-01-14 18:51:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='personal-assistant',
+    version='1.0.0',
+    author='Vadym Mykhailiukov&Team#10',
+    author_email='vidam1973@ukr.net',
+    description='Персональний помічник для управління контактами, нотатками та файлами.',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # Список залежностей, якщо вони є. Наприклад:
+        # 'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'personal-assistant=main:main',  # Точка входу до вашої програми
+        ],
+    },
+)
 

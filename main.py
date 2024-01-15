bc6eb2d721e7ae129ce7c79ca0f6c43c67422b09
Oleksandr <kivilele@gmail.com>
2024-01-15 13:35:01 +0200

    Updated

--- main.py
+++ main.py
@@ -18,22 +18,35 @@ def main():
         if command == "exit":
             break
 
-        elif command == 'create_note':
-            # Запрашуємо ввести текст нотатки 
-            info = input(">>>Введіть текст нотатки:")
-            # Додає нотатку в БД 
-            note_manager.add_note(info)
-            # Показує БД
-            note_manager.show_data()
+        try:
+            command_arg = lst_of_args[1]
+            #Обробка випадку коли користувач не ввів аргумент для command
+        except IndexError:
+            print('Команду для мендежеру нотаток не передано')
+            continue
         
-        elif command == 'add_tag':
-            # Запрашуємо id нотатки
-            note_id = input(">>>Введіть ID нотатки, до якої хочете додати тег:")
-            # Запрашуємо тег
-            tag = input(">>>Введіть тег:")
-            # Додаємо тег в нотатку
-            note_manager.add_tag(int(note_id), tag)
+
+        if command == 'note':
+            if command_arg == 'create':
+                # Запрашуємо ввести текст нотатки 
+                info = input(">>>Введіть текст нотатки:")
+                # Додає нотатку в БД 
+                note_manager.add_note(info)
+                print('Нотатку зроблено')
+                
         
+            elif command_arg == 'add_tag':
+                # Запрашуємо id нотатки
+                note_id = input(">>>Введіть ID нотатки, до якої хочете додати тег:")
+                # Запрашуємо тег
+                tag = input(">>>Введіть тег:")
+                # Додаємо тег в нотатку
+                note_manager.add_tag(int(note_id), tag)
+                print("Тег додано")
+
+            elif command_arg == 'data':
+                note_manager.show_data()
+            
         # Це маленька чатсина функціоналу - просто приклад
 if __name__ == '__main__':
     main()     
